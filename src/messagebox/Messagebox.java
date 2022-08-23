9d27b78bfe29830899205c5114fa4dee1ad1549e
ShohagHassan <93638395+ShohagHassan@users.noreply.github.com>
2022-08-23 11:24:45 -0400

    Add files via upload

---
+++ src/messagebox/Messagebox.java
@@ -0,0 +1,14 @@
+
+package messagebox;
+
+import javax.swing.JOptionPane;
+
+
+public class Messagebox {
+
+    public static void main(String[] args) {
+  
+        JOptionPane.showConfirmDialog(null,"Do you want to quit","Title",JOptionPane.YES_NO_OPTION);
+    }
+    
+}
